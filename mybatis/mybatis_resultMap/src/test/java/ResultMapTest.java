198abc74946641a710222168d0b444a7887aa653
STONERAIN <STONERAIN@qq.com>
2023-04-15 10:14:56 +0800

    SSM

--- mybatis/mybatis_resultMap/src/test/java/ResultMapTest.java
+++ mybatis/mybatis_resultMap/src/test/java/ResultMapTest.java
@@ -6,14 +6,26 @@ import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
 public class ResultMapTest {
+
+    /**
+     * 使一级缓存失效的四种情况：
+     * 1) 不同的SqlSession对应不同的一级缓存
+     * 2) 同一个SqlSession但是查询条件不同
+     * 3) 同一个SqlSession两次查询期间执行了任何一次增删改操作 --->可能影响缓存中的数据
+     * 4) 同一个SqlSession两次查询期间手动清空了缓存
+     */
     @Test
     public void test1(){
         SqlSession sqlSession = SqlSessionUtil.getSqlSession();
         EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
         Emp empByEmpId = mapper.getEmpByEmpId(1);
-        System.out.println(empByEmpId);
-        Dept dept = empByEmpId.getDept();
-        System.out.println(dept);
+        Emp empByEmpId1 = mapper.getEmpByEmpId(1);
+        System.out.println(empByEmpId == empByEmpId1);
+        SqlSession sqlSession1 = SqlSessionUtil.getSqlSession();
+        EmpMapper mapper1 = sqlSession1.getMapper(EmpMapper.class);
+        Emp empByEmpId2 = mapper1.getEmpByEmpId(1);
+        System.out.println(empByEmpId == empByEmpId2);
         sqlSession.close();
+        sqlSession1.close();
     }
 }
