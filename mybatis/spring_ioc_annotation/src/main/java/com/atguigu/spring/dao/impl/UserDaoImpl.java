8776b4b1f4959b1edf75abda27de87c4ff3dbd65
STONERAIN <STONERAIN@qq.com>
2023-04-16 10:39:03 +0800

    SSM

---
+++ mybatis/spring_ioc_annotation/src/main/java/com/atguigu/spring/dao/impl/UserDaoImpl.java
@@ -0,0 +1,6 @@
+package com.atguigu.spring.dao.impl;
+
+import com.atguigu.spring.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+}
---
+++ mybatis/spring_ioc_annotation/src/main/java/com/atguigu/spring/service/impl/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package com.atguigu.spring.service.impl;
+
+import com.atguigu.spring.service.UserService;
+
+public class UserServiceImpl implements UserService {
+}
